6811558484cd51ae89638b98a1c4c12ba2479a84
Johny <jonnycrazy776@gmail.com>
2025-05-04 15:40:32 +0500

    added_main_page

--- besmart/besmart/urls.py
+++ besmart/besmart/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('main/', include('main.urls')),
 ]
 
 if settings.DEBUG:
