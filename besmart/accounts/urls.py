c10a0be0b8776da96d70cf69ce65d6af889ecb26
Johny <jonnycrazy776@gmail.com>
2025-05-08 05:54:13 +0500

    added upload photo and video and some updates on home page

--- besmart/accounts/urls.py
+++ besmart/accounts/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('profile/', views.profile_view, name='profile_view'),
     path('update-profile/', views.update_profile, name='update_profile'),
+    path('upload_content/', views.upload_content, name='upload_content'),
+    
 ]
+
--- besmart/main/models.py
+++ besmart/main/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import Content
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscriber')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator')
+    timestamp = models.DateTimeField(auto_now_add=True)
