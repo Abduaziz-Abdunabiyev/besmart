eb2b8eb17b527503cf82cd1081ec0558b493c904
Johny <jonnycrazy776@gmail.com>
2025-05-10 07:50:31 +0500

    updated like function but there is some issue any way code working for now

--- besmart/main/views.py
+++ besmart/main/views.py
@@ -9,31 +9,51 @@ def home(request):
     contents = Content.objects.select_related('user').order_by('-upload_time')
     return render(request, 'main/home_test.html', {'contents': contents})
 
-
-# accounts/views.py
-
 from django.shortcuts import redirect, get_object_or_404
 from .models import Comment, Subscription
 from django.contrib.auth.decorators import login_required
 
-# views.py
+
+from django.http import HttpResponse
 from django.http import JsonResponse
 from main.models import Content
 from accounts.models import Like
 
 @login_required
+# def like_content(request, content_id):
+#     content = get_object_or_404(Content, id=content_id)
+
+#     # Create a Like only if it doesn't already exist
+#     like, created = Like.objects.get_or_create(user=request.user, content=content)
+
+#     if created:
+#         content.likes += 1  # optional
+#         content.save()
+
+#     total_likes = Like.objects.filter(content=content).count()
+#     return HttpResponse(status=204)
 def like_content(request, content_id):
     content = get_object_or_404(Content, id=content_id)
 
-    # Create a Like only if it doesn't already exist
-    like, created = Like.objects.get_or_create(user=request.user, content=content)
+    # Check if the user has already liked the content
+    like = Like.objects.filter(user=request.user, content=content).first()
 
-    if created:
-        content.likes += 1  # optional
-        content.save()
+    if like:
+        # If the user has already liked the content, remove the like (unlike)
+        like.delete()
+        action = "unliked"
+    else:
+        # If the user has not liked the content yet, add the like
+        Like.objects.create(user=request.user, content=content)
+        action = "liked"
 
-    total_likes = Like.objects.filter(content=content).count()
-    return JsonResponse({'likes': total_likes})
+    total_likes = Like.objects.filter(content=content).count()  # Get the updated like count
+
+    # Return a JSON response with the updated like count and action (liked/unliked)
+    return JsonResponse({
+        'total_likes': total_likes,
+        'action': action
+    })
 
 @login_required
 def comment_content(request, content_id):
